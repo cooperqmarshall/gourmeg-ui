6e4dc4a827e1d57082fc141d0aff5304e39c5e13
cooper <cooperqmarshall@gmail.com>
2022-10-07 19:18:31 -0700

    Added Sorting dropdown with options `Most Viewed` and `Newest` with ascending and descending icon toggle.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,43 @@
     <div class="container">
       <h1 class="title is-2 has-text-centered">Home</h1>
       <AddRecipe />
+      <div class="block" style="display: flex; align-items: center">
+        <div
+          class="dropdown"
+          @click="listDropdownActive = !listDropdownActive"
+          :class="{ 'is-active': listDropdownActive }"
+        >
+          <div class="dropdown-trigger">
+            <button
+              class="button"
+              aria-haspopup="true"
+              aria-controls="dropdown-menu"
+            >
+              <span>{{ this.sort == "views" ? "Most Viewed" : "Newest" }}</span>
+              <span class="icon is-small">
+                <i class="fas fa-angle-down" aria-hidden="true"></i>
+              </span>
+            </button>
+          </div>
+          <div class="dropdown-menu" id="dropdown-menu" role="menu">
+            <div class="dropdown-content">
+              <a href="#" @click="this.sort = 'views'" class="dropdown-item"
+                >Most Viewed</a
+              >
+              <a href="#" @click="this.sort = 'id'" class="dropdown-item"
+                >Newest</a
+              >
+            </div>
+          </div>
+        </div>
+        <span
+          @click="this.order = this.order == 'up' ? 'down' : 'up'"
+          class="icon is-size-5"
+          style="padding-left: 0.5rem"
+        >
+          <i class="fas" :class="'fa-sort-amount-' + this.order"></i>
+        </span>
+      </div>
       <div class="columns is-multiline is-centered">
         <RecipeCard v-for="r in recipes" :recipe="r" :key="r.id" />
       </div>
@@ -21,16 +58,20 @@ export default {
     RecipeCard,
     AddRecipe,
   },
-  // data() {
-  //  return {
-  //  };
-  // },
+  data() {
+    return { listDropdownActive: false, sort: "new", order: "down" };
+  },
   mounted() {
     this.$store.dispatch("getRecipes");
   },
   computed: {
     recipes() {
-      return _.orderBy(this.$store.state.recipes, "id", "desc");
+      console.log("here");
+      return _.orderBy(
+        this.$store.state.recipes,
+        this.sort,
+        this.order == "up" ? "desc" : "asc"
+      );
     },
   },
 };
